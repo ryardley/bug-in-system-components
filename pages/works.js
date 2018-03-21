4d9dc528e1f5227d59d54498e12ebf3e09aaa29f
Rudi Yardley <contact@rudiyardley.com>
2018-03-22 08:36:39 +1100

    Add examples where styled-system is not part of the equation

--- pages/works.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import {space, color} from 'styled-system';
-
-const Comp = styled.div`${space}${color}`;
-
-const Box = (props) => <Comp p={2} bg='green' {...props} />
-
-export default () => <Box>This causes no error!</Box>
